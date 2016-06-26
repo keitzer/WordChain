f8c9d5421cab9ecee32da8d2d00f373ece224418
daniel ogorek <ogorek@usc.edu>
2016-06-26 10:30:51 -0400

    more 2 letter tests

--- WordChainTests/ChainTests.swift
+++ WordChainTests/ChainTests.swift
@@ -21,14 +21,14 @@ class ChainTests: QuickSpec {
             }
             
             describe("with 1 letter") {
-                it("should be able to chain to the same letter in 1 step") {
+                it("should chain to the same letter in 1 step") {
                     let result = subject.wordChain(withStartingWord: "A", withEndingWord: "A")
                     expect(result.count).to(equal(2))
                     expect(result[0]).to(equal("A"))
                     expect(result[1]).to(equal("A"))
                 }
                 
-                it("should be able to go from A to I in 1 step") {
+                it("should chain from A to I in 1 step") {
                     let result = subject.wordChain(withStartingWord: "A", withEndingWord: "I")
                     expect(result.count).to(equal(2))
                     expect(result[0]).to(equal("A"))
@@ -37,11 +37,26 @@ class ChainTests: QuickSpec {
             }
             
             describe("with 2 letters") {
-                it("should be able to go chain the same word in 1 step") {
+                it("should chain to the same word in 1 step") {
                     let result = subject.wordChain(withStartingWord: "AM", withEndingWord: "AM")
                     expect(result.count).to(equal(2))
                     expect(result[0]).to(equal("AM"))
                     expect(result[1]).to(equal("AM"))
+                }
+                
+                it("should chain to a word with 1 letter difference in 1 step") {
+                    let result = subject.wordChain(withStartingWord: "IF", withEndingWord: "IS")
+                    expect(result.count).to(equal(2))
+                    expect(result[0]).to(equal("IF"))
+                    expect(result[1]).to(equal("IS"))
+                }
+                
+                it("should chain from IS to AM in 2 steps") {
+                    let result = subject.wordChain(withStartingWord: "IS", withEndingWord: "AM")
+                    expect(result.count).to(equal(3))
+                    expect(result[0]).to(equal("IS"))
+                    expect(result[1]).to(equal("AS"))
+                    expect(result[2]).to(equal("AM"))
                 }
             }
         }
